a9be4368dd4c7e19f96174ec3cad58d952887f5f
Felipe Lopes de Oliveira <33868364+lipelopesoliveira@users.noreply.github.com>
2023-11-01 18:32:03 -0300

    Fix missing functions on import (#4)

--- src/pymser/__init__.py
+++ src/pymser/__init__.py
@@ -4,10 +4,13 @@ from .pymser import (exp_decay,
                      calculate_MSEm,
                      MSERm_index,
                      MSERm_LLM_index,
+                     enthalpy_of_adsorption,
                      calc_equilibrated_average,
+                     calc_equilibrated_enthalpy,
                      calc_autocorrelation_time,
                      apply_ADF_test,
-                     equilibrate)
+                     equilibrate,
+                     equilibrate_enthalpy)
 
 __all__ = [exp_decay,
            check_consistency,
@@ -15,6 +18,10 @@ __all__ = [exp_decay,
            calculate_MSEm,
            MSERm_index,
            MSERm_LLM_index,
+           enthalpy_of_adsorption,
            calc_equilibrated_average,
+           calc_equilibrated_enthalpy,
            calc_autocorrelation_time,
-           apply_ADF_test, equilibrate]
+           apply_ADF_test,
+           equilibrate,
+           equilibrate_enthalpy]
